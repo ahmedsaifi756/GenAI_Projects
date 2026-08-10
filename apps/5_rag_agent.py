624a0065f11d54f9bc5d87d5222f2326fbcdc71f
mohd ahmed <ahmedsaifi756@gmail.com>
2026-08-11 00:46:44 +0530

    formatting text

--- apps/5_rag_agent.py
+++ apps/5_rag_agent.py
@@ -1,13 +1,24 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-
 from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 from langchain_groq import ChatGroq
-from langchain_community.vectorstores import InMemoryVectorStore
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langgraph.checkpoint.memory import InMemorySaver
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 import streamlit as st
+
+loader = PyPDFLoader("data/namer.pdf")
+documents = loader.load()
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
